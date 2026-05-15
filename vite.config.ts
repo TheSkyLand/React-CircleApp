c336ad793fbea5ba22ff611a9845d8c7f5534a7e
TheSkyLand <Aradioactiveplay@gmail.com>
2026-05-15 19:51:49 +0300

    fixed vite.config.ts file

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default defineConfig(
-  {
-    plugins: [react()],
-    server: {
-      port: 3000, // Порт для локальной разработки
-      open: true, // Автоматическое открытие в браузере
+export default defineConfig({
+  base: '/', // 👈 СТРОГО ОБЯЗАТЕЛЬНО: указывает Vite собирать пути от корня сайта
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'), // 👈 Связываем алиас '@' с папкой 'src'
-      },
-    }
-  }
-)
+  },
+})
